7acbc5b8dbf1b5aa84962917472a90bf90f5b4df
Vinodkumar Yerraballi <98636972+Vinodkumar-yerraballi@users.noreply.github.com>
2022-07-10 14:23:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from sklearn.linear_model import LinearRegression
 label=LabelEncoder()
 
 @st.cache
-image1=Image.open('acura.jpeg')
+image1=Image.open('Acura.jpeg')
 image2=Image.open('Audi.jpeg')
 image3=Image.open('BMW.jpeg')
 image4=Image.open('Buick.jpeg')
